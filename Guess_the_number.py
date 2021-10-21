91a09eecffb2b7344bda34d8f71e026b5588e28f
L00170298 <L00170298@student.lyit.ie>
2021-10-21 17:17:14 +0100

    Secondary Commit

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -5,17 +5,25 @@
 # Q1 - Guessing Game
 """
 
+from gues_number_helper import isNotEqual
+
 print("Guess the number\n")
 hidden_number = int(input("Player 1: Pick a number from 1 to 10 for player 2 to guess: "))
 print("\n" * 100)
 print("Player 2: Guess the number that player 1 picked\n")
+
 attempt = int(input("Enter your guess:  "))
+
 count = 1
-while attempt != hidden_number:
-    count += 1
-    print("!" * 50)
-    attempt = int(input("Incorrect. Try again: "))
-    print("You've entered",count , "attempts")
+while isNotEqual(attempt, hidden_number):
+    try :
+        count += 1
+        print("!" * 50)
+        attempt = int(input("Incorrect. Try again: "))
+        print("You've entered",count , "attempts")
+    except ValueError:
+        print("Oops! That was not valid number. Try again..."
+        
 print("\n" * 10)
 print("="*50)
 print("You're correct, the hidden number was", hidden_number)
