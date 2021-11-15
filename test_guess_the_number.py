9d19bef0e91518c226ff8a0999de667e7739369a
L00170298 <91154609+Nikolajs2021@users.noreply.github.com>
2021-11-15 20:08:35 +0000

    Update test_guess_the_number.py

--- test_guess_the_number.py
+++ test_guess_the_number.py
@@ -1,13 +1,13 @@
-from gues_number_helper import isNotEqual, isInteger
+from gues_number_helper import isNotEqual, isInteger # import the functions for test
 
-import unittest
+import unittest # call the function unittest
 
-class TestInputValidation(unittest.TestCase):
-    def test_not_equlal_values(self):
-        self.assertTrue(isNotEqual(2, 3))
-    def test_equal_valus(self):
-        self.assertFalse(isNotEqual(2, 2))
-    def test_isInt(self):
-        self.assertTrue(isInteger(1))
-    def test_isNotInt(self):
-        self.assertFalse(isInteger('not int'))
+class TestInputValidation(unittest.TestCase): # creating for the test, testing the functions with different values
+    def test_not_equlal_values(self):         # expected output TRUE when not equales pass
+        self.assertTrue(isNotEqual(2, 3))     # the function acceptin 2 int and when it != returns TRUE
+    def test_equal_valus(self):               # expected output FALSE when the values are equale
+        self.assertFalse(isNotEqual(2, 2))    # the function acceptin 2 int and when it == returns FALSE
+    def test_isInt(self):                     # checking the int and except one value
+        self.assertTrue(isInteger(1))         # return TRUE when int pass
+    def test_isNotInt(self):                  # checking the is not int and excepts one value
+        self.assertFalse(isInteger('not int'))# return FALSE when  none int pass
